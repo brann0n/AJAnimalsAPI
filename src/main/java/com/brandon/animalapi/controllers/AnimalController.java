b766a5ac250f3885662cbebd8622cb02fd380e30
Brandon <43607839+brann0n@users.noreply.github.com>
2021-12-07 15:39:14 +0100

    fixed a dto

--- src/main/java/com/brandon/animalapi/controllers/AnimalController.java
+++ src/main/java/com/brandon/animalapi/controllers/AnimalController.java
@@ -6,6 +6,8 @@ import com.brandon.animalapi.services.AnimalService;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
